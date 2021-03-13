c5fdbb03798136124023d0260ebc15a2fa261bf1
hbrandon15 <hernandezbrandon17@gmail.com>
2021-03-13 12:35:45 -0600

    Completed Exercise 3: Making Matrix using Arrays

--- PointersAndArrays/Exercises/fiveByFiveArray2.cpp
+++ PointersAndArrays/Exercises/fiveByFiveArray2.cpp
@@ -6,14 +6,21 @@ using namespace std;
 int main(){
    int grid[5][5]; 
    
-   for(int i = 0; i < 5; i++){
-       for(int j = 0; j < 5; j++){
-           if((i == 0 && j == 0) || (i == 1 && j == 1) || (i == 2 && j == 2) || (i == 3 && j == 3) || (i == 4 && j == 4)){
-               grid[i][j] = 1; 
+   for(int i = 0; i < 5; i++){ // loop for the rows
+
+       for(int j = 0; j < 5; j++){ // loop for the columns
+
+           if((i == 0 && j == 0) || (i == 1 && j == 1) || (i == 2 && j == 2) || (i == 3 && j == 3) || (i == 4 && j == 4)){ // setting the diagonal equal to 0
+               grid[i][j] = 0; 
+               
+           }
+           else if((i > 0 && j == 0) || ( i > 1 && j == 1) || (i > 2 && j == 2) || (i > 3 && j == 3)){ // setting below diagonal to -1
+             grid[i][j] = -1;
+             
            }
            else
-             grid[i][j] = 0;
-           cout << grid[i][j] << " ";  
+             grid[i][j] = 1; // setting the rest equal to 1
+           cout << grid[i][j] << " ";  // printing my grid
        }
        cout << endl; 
    }
---
+++ PointersAndArrays/Exercises/pascalTriangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; 
+
+void printPascalTr(int size){
+  int i, j;
+
+  int grid[size][size];
+
+  for(i = 0; i < size; i++){
+
+    for(j = 0; j < size; j++){
+
+    }
+  }
+
+
+}
+int main(){
+
+  // printPascalTr(5);
+
+
+
+
+  return 0; 
+}
