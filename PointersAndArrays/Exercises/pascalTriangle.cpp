755baac7e7e84cc16e8bbe01ae457cd5fd9b8412
hbrandon15 <hernandezbrandon17@gmail.com>
2021-03-15 12:17:49 -0500

    Pascal Triangle Exercise

--- PointersAndArrays/Exercises/pascalTriangle.cpp
+++ PointersAndArrays/Exercises/pascalTriangle.cpp
@@ -10,15 +10,29 @@ void printPascalTr(int size){
   for(i = 0; i < size; i++){
 
     for(j = 0; j < size; j++){
+      if(j == 0){
+        grid[i][j] = 1; 
+      }
+      else if(i == 1 && j == 1){
+        grid[i][j] = 1;
+      }
+      else if(i >= 2 && j == 1){
+        grid[i][j] = i;
+      }
+      else{
+        grid[i][j] = 0;
+      }
+      cout << grid[i][j] << " ";
 
     }
+    cout << endl; 
   }
 
 
 }
 int main(){
 
-  // printPascalTr(5);
+  printPascalTr(5);
 
 
 
