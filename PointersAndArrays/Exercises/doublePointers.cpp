2eaeaba629cf9ec0a62a7ec3a603aa0937bfccfd
hbrandon15 <hernandezbrandon17@gmail.com>
2021-03-16 16:45:38 -0500

    MakingAboardExercise

---
+++ PointersAndArrays/Exercises/doublePointers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std; 
+
+
+void printBoard(){
+
+
+}
+
+
+void makeBoard(int x, int y){
+  int *ptr1 = new int;
+  int **ptr2;
+  ptr2 = &ptr1;
+
+
+
+
+
+
+
+}
+
+int main(){
+
+
+  return 0;
+}
