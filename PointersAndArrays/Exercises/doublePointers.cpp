bd39adebf3d5285b313ae69fc713129cc37f3bc0
hbrandon15 <hernandezbrandon17@gmail.com>
2021-03-18 13:44:41 -0500

    Implementing Double Pointers

--- PointersAndArrays/Exercises/doublePointers.cpp
+++ PointersAndArrays/Exercises/doublePointers.cpp
@@ -3,27 +3,32 @@
 using namespace std; 
 
 
-void printBoard(){
-
-
+void printBoard(int **board,int x, int y) {
+    for (int i = 0; i < x; ++i) {   // for each row
+        for (int j = 0; j < y; ++j) { // for each column
+            board[i][j] = 1;
+            cout << board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
 
+void makeBoard(int x,int y) {
+    int **board;  // this is our second pointer
 
-void makeBoard(int x, int y){
-  int *ptr1 = new int;
-  int **ptr2;
-  ptr2 = &ptr1;
+    board = new int*[x]; // We have initialized our pointer to a new integer pointer array of size X
 
+    for (int i = 0; i < x; ++i) {
+        board[i] = new int[y]; // basically we are putting one array into another and making a multidim array
+        // after making our constructed array, we can now print the board which will then populate the board. 
+    }
 
-
-
-
-
+    printBoard(board,x,y);
 
 }
 
 int main(){
 
-
+  makeBoard(5,4);
   return 0;
 }
